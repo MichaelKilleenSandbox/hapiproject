eb772126787c27c87609e0b4c806de8b8d9a4f04
Michael Killeen <michael.j.killeen@outlook.com>
2016-10-08 18:24:47 -0400

    Latest Hapi JS changes. Most around routes.

--- hapi.js
+++ hapi.js
@@ -5,11 +5,29 @@ const Hapi = require('hapi');
 const Blipp = require('blipp');
 const Joi = require('joi');
 const Inert = require('inert');
+const Boom = require('boom');
+const Vision = require('vision');
+
 const server = new Hapi.Server();
 
+const hello = function(name) {
+    return this.response({hello:name});
+}
+
+// Defines a new handler for routes on this server.
+server.decorate('reply','hello',hello);
+
 server.connection({
-    port:1337,
-    host:'localhost'
+    port: 1337,
+    host: 'localhost'
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        return reply.hello(request.params.name);
+    }
 });
 
 // Routes
@@ -24,39 +42,46 @@ server.route({
             }
         },
         pre: [],
-        handler: function(request,reply) {
+        handler: function (request, reply) {
             const name = request.params.name;
             return reply({message: `Hello ${name}`});
         },
         cache: {
-            expiresIn:3600000
+            expiresIn: 3600000
         }
     }
 });
 
 server.route({
-        method: 'GET',
-        path: '/partslookup',
+    method: 'GET',
+    path: '/partslookup',
     config: {
         description: 'Return an object with hello message',
         pre: [],
-        handler: function(request,reply) {
+        handler: function (request, reply) {
             reply.file('testdata.json');
         },
         cache: {
-            expiresIn:3600000
+            expiresIn: 3600000
         }
-        }
-       });
+    }
+});
 
+server.route({
+    method: '*',
+    path: '/{p*}',
+    handler: function(request, reply) {
+        return reply('The page was not found:' + request.path).code(404);
+    }
+});
 
 // Register plugins and such...
-server.register([Blipp,Inert], (err)=> {
-    if(err) {
+server.register([Blipp, Inert], (err)=> {
+    if (err) {
         throw err;
     }
     server.start((err)=> {
-        if(err) {
+        if (err) {
             throw err;
         }
         console.log(`Server running at ${server.info.uri}`);
